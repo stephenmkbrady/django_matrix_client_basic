02944eed1a8d039a6637d79770e91108258bf009
stephen brady <stephenmkbrady@protonmail.com>
2018-05-13 23:22:37 +0100

    set default=1 for IntegerField in model as there is invalid int error thrown on first save where no int being modified

--- client_app/models.py
+++ client_app/models.py
@@ -5,7 +5,7 @@ class Session(models.Model):
 	matrix_client = models.CharField(max_length = 100)
 	matrix_user_name = models.CharField(max_length = 100)
 	matrix_room_name = models.CharField(max_length = 100, default="")
-	message_count = models.IntegerField(default="")
+	message_count = models.IntegerField(default= 1)
 	# matrix session token
 	matrix_token = models.CharField(max_length = 200)
 	# matrix "time" token, stored with every request
