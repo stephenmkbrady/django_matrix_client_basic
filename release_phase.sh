ccf417c741110d7f93a1b9ba6f9d03815daf5a96
stephen brady <stephenmkbrady@protonmail.com>
2018-05-16 01:57:06 +0100

    removed testing from release phase

--- release_phase.sh
+++ release_phase.sh
@@ -3,7 +3,7 @@
 echo "Running Release Tasks"
 echo "collectstatic"
 python manage.py collectstatic
-echo "Running Tests"
-python manage.py test
+# echo "Running Tests"
+# python manage.py test
 echo "Running Migrations"
 python manage.py migrate
